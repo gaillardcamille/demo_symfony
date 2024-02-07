4ba1eb060e306fc6f6823e07097c85ab27e32328
gaillardcamille <gaillard.camille48@gmail.com>
2024-02-07 17:35:24 +0100

    fixtures avec faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,19 +5,22 @@ namespace App\DataFixtures;
 use App\Entity\Manga;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
 
-        $manga = new Manga();
-        $manga->setNom("One Piece");
-        $manga->setCategorie("Aventure");
-        $manga->setDescription("blblblbl");
-        $manager->persist($manga);
+        for ($i = 0; $i < 15; $i++) {
+            $manga[$i] = new Manga();
+            $manga[$i]->setNom($faker->name());
+            $manga[$i]->setCategorie($faker->word);
+            $manga[$i]->setDescription($faker->sentence(20));
+            $manager->persist($manga[$i]);
+        }
+
 
         $manager->flush();
     }
