b38861609b3b47b4075ed3664dd9fc01f1003ce8
gaillardcamille <gaillard.camille48@gmail.com>
2024-02-07 17:00:24 +0100

    crud fonctionnelle

---
+++ src/Form/MangaType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Manga;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MangaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Nom')
+            ->add('Categorie')
+            ->add('Description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Manga::class,
+        ]);
+    }
+}
---
+++ tests/Controller/MangaControllerTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Manga;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MangaControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private EntityManagerInterface $manager;
+    private EntityRepository $repository;
+    private string $path = '/manga/';
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->manager = static::getContainer()->get('doctrine')->getManager();
+        $this->repository = $this->manager->getRepository(Manga::class);
+
+        foreach ($this->repository->findAll() as $object) {
+            $this->manager->remove($object);
+        }
+
+        $this->manager->flush();
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Manga index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+    }
+
+    public function testNew(): void
+    {
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'manga[Nom]' => 'Testing',
+            'manga[Categorie]' => 'Testing',
+            'manga[Description]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects('/sweet/food/');
+
+        self::assertSame(1, $this->getRepository()->count([]));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Manga();
+        $fixture->setNom('My Title');
+        $fixture->setCategorie('My Title');
+        $fixture->setDescription('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Manga');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Manga();
+        $fixture->setNom('Value');
+        $fixture->setCategorie('Value');
+        $fixture->setDescription('Value');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'manga[Nom]' => 'Something New',
+            'manga[Categorie]' => 'Something New',
+            'manga[Description]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/manga/');
+
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getNom());
+        self::assertSame('Something New', $fixture[0]->getCategorie());
+        self::assertSame('Something New', $fixture[0]->getDescription());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Manga();
+        $fixture->setNom('Value');
+        $fixture->setCategorie('Value');
+        $fixture->setDescription('Value');
+
+        $this->manager->remove($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertResponseRedirects('/manga/');
+        self::assertSame(0, $this->repository->count([]));
+    }
+}
